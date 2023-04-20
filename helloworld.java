b33331e8e967cf4294f09454fc929487874db10f
Fathima <31350267+fathizjm@users.noreply.github.com>
2023-04-21 02:47:30 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,8 @@
-class helloworld {
+class hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        int a = 9;
+        int b = 7;
+        int c = a+b;
+        System.out.println("Addition of " + a + " and " + b + " is " + c); 
     }
 }
